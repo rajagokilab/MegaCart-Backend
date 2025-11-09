19acf728f307133430990833e6742ef1729325a8
rajagokilab <rajagokilab@gmail.com>
2025-11-09 14:03:59 +0100

    settingsemail

--- myproject/settings.py
+++ myproject/settings.py
@@ -174,3 +174,14 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rajagokilavivek@gmail.com'
 EMAIL_HOST_PASSWORD = 'eaaw smdg xdtz gnhv'  # Use App Password for Gmail
 DEFAULT_FROM_EMAIL = 'MegaCart Support <rajagokilavivek@gmail.com>'
+
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','rajagokilavivek@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','eaaw smdg xdtz gnhv')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', f"MegaCart Support <{EMAIL_HOST_USER}>")
