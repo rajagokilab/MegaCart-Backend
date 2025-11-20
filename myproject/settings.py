1d2ddd893a048d96f29c471a5721f30cd95380ee
rajagokilab <rajagokilab@gmail.com>
2025-11-20 20:57:04 +0100

    image cloudinary problem fixed

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # --- SECURITY ---
 SECRET_KEY = 'django-insecure-sa14nzjk(&%vtlek)#+r^uyo2se3jturn*xp^0bj%y=*449=p('
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # --- INSTALLED APPS ---
